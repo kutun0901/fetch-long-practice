c4de9f42dca0d0ab5e0f0bf74c0d01f3c1b9ecf8
kutun0901 <tuan.tq2433@gmail.com>
2022-12-14 13:29:51 -0800

    phrase 1 done

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,6 +6,11 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+fetch('/products', {
+    method: 'POST',
+    headers: {'Content-Type': "application/x-www-form-urlencoded"},
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery"
+})
 
 
 
